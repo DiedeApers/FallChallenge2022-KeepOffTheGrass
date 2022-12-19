feefc71a789cae3ad03ca7dce32854409379128a
DiedeApers <diedeapers@gmail.com>
2022-12-20 00:19:30 +0100

    run 50 times to test

--- src/main/java/com/codingame/KeepOffTheGrassMain.java
+++ src/main/java/com/codingame/KeepOffTheGrassMain.java
@@ -11,19 +11,26 @@ public class KeepOffTheGrassMain {
 
     public static void main(String[] args) throws IOException, InterruptedException {
 
-        MultiplayerGameRunner gameRunner = new MultiplayerGameRunner();
 
-        //gameRunner.setSeed(-8358938852454912011l);
-        
-        gameRunner.addAgent("./build/output.exe", "JulienBot");
-        //gameRunner.addAgent("./build/idle.exe", "IdleBot");
-        gameRunner.addAgent("python ./tools/FallChallenge2022-KeepOffTheGrass/starterAIs/Starter.py", "InsectBot");
+        for (int i = 0; i < 50; i++) 
+        {
+            MultiplayerGameRunner gameRunner = new MultiplayerGameRunner();
 
-        //gameRunner.start();
-        GameResult result = gameRunner.simulate();
-        System.out.println("Agents: " + result.agents); 
-        System.out.println("Scores: " + result.scores); 
-        System.out.println("Summaries: " + result.summaries); 
+            //gameRunner.setSeed(5l);
+            
+            gameRunner.addAgent("./build/output.exe", "JulienBot");
+            //gameRunner.addAgent("./build/idle.exe", "IdleBot");
+            gameRunner.addAgent("python ./tools/FallChallenge2022-KeepOffTheGrass/starterAIs/Starter.py", "InsectBot");
+
+            //gameRunner.start();
+            GameResult result = gameRunner.simulate();
+            //aSystem.out.println("SEED: " + gameRunner.getSeed()); 
+            System.out.println("Agents: " + result.agents); 
+            System.out.println("Scores: " + result.scores); 
+            System.out.println("Summaries: " + result.summaries); 
+        }
+
+        System.out.println("SUCCESS!"); 
     }
 
 }
